b3b3806c667e29ad485c30553a72f2be6819012c
Dezarea Bryan <dezareabryan@gmail.com>
2023-05-18 17:18:18 -0400

    Uploaded css

--- template-application/src/App.jsx
+++ template-application/src/App.jsx
@@ -1,15 +1,51 @@
-import React, {useState} from 'react'
-import { BrowserRouter, Routes, Route, Link} from "react-router-dom"
-import Home from "./components/Home"
-import About from "./components/About"
-import Projects from "./components/Projects"
-import Contact from "./components/Contact"
-import Error from "./components/Error"
-import "./App.css"
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import "./App.css";
 
 const App = () => {
+  const [activeNavItem, setActiveNavItem] = useState("");
+
+  const handleNavItemClick = (navItem) => {
+    setActiveNavItem(navItem);
+  };
+
   return (
     <BrowserRouter>
+      <nav>
+        <Link
+          to="/"
+          onClick={() => handleNavItemClick("Home")}
+          className={`nav-item ${activeNavItem === "Home" ? "active" : ""}`}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          onClick={() => handleNavItemClick("About")}
+          className={`nav-item ${activeNavItem === "About" ? "active" : ""}`}
+        >
+          Our Team
+        </Link>
+        <Link
+          to="/projects"
+          onClick={() => handleNavItemClick("Projects")}
+          className={`nav-item ${activeNavItem === "Projects" ? "active" : ""}`}
+        >
+          Projects
+        </Link>
+        <Link
+          to="/contact"
+          onClick={() => handleNavItemClick("Contact")}
+          className={`nav-item ${activeNavItem === "Contact" ? "active" : ""}`}
+        >
+          Contact Us
+        </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -17,8 +53,9 @@ const App = () => {
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <footer className="sticky-footer">This is the footer.</footer>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
